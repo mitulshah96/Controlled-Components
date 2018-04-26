722ef71d8d921e54544fe82de68acef092d7f932
Mitul Shah <mitulshah96@gmail.com>
2018-04-26 13:08:01 +0530

    controlled/uncontrolled components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,67 @@
 import React, { Component } from "react";
 import "./App.css";
 
-//Controlled Components
-/* React component that renders a form also controls what happens in that form on subsequent user 
-input. An input form element whose value is controlled by React in this way is called a 
-“controlled component”. */
-
 class App extends Component {
+  // Likewise, <input type="checkbox"> and <input type="radio"> support defaultChecked,
+  // and <select> and <textarea> supports defaultValue.
+
+  // In React, an <input type="file" /> is always an uncontrolled component because
+  //  its value can only be set by a user, and not programmatically.
+
   constructor(props) {
     super(props);
     this.state = {
-      value: "",
-      selectedvalue: "Mango"
+      isGoing: true,
+      numberOfGuests: 2
     };
   }
+  handleInputChange(e) {
+    const target = e.target;
+    console.log(target);
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    console.log(value);
+    const name = target.name;
+    console.log(name);
 
-  handleChange(e) {
     this.setState({
-      value: e.target.value.toUpperCase()
-    });
-  }
-  handleSelect(e) {
-    this.setState({
-      selectedvalue: e.target.value
+      [name]: value
     });
   }
   handleSubmit(e) {
     e.preventDefault();
-    console.log(this.state.value);
+    console.log(this.input.value);
   }
-
   render() {
     return (
       <div className="App">
         <form onSubmit={e => this.handleSubmit(e)}>
           <label>
-            Name:
+            Is going:
             <input
-              type="text"
-              value={this.state.value}
-              onChange={e => this.handleChange(e)}
+              name="isGoing"
+              type="checkbox"
+              checked={this.state.isGoing}
+              onChange={e => this.handleInputChange(e)}
             />
           </label>
           <br /> <br /> <br /> <br />
-          <select
-            value={this.state.selectedvalue}
-            onChange={e => this.handleSelect(e)}
-          >
-            <option value="Grapes">Grapes</option>
-            <option value="Oranges">Oranges</option>
-            <option value="Mango">Mango</option>
-            <option value="Dragon Fruit">Dragon Fruit</option>
-            <option value="Kivi">Kivi</option>
-          </select>
+          <label>
+            Number of guests:
+            <input
+              name="numberOfGuests"
+              type="number"
+              value={this.state.numberOfGuests}
+              onChange={e => this.handleInputChange(e)}
+            />
+          </label>
+          <label>
+            uncontrolled Component
+            <input
+              type="text"
+              defaultValue="Shah"
+              ref={input => (this.input = input)}
+            />
+          </label>
           <input type="submit" value="submit" />
         </form>
       </div>
